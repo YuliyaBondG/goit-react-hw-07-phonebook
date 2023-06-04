73aef46637c98afebb4160355a8661625aa20b63
YuliyaBondG <manyunka@ukr.net>
2023-06-04 22:26:56 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,10 @@ export const App = () => {
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
-
       <ContactForm />
-
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
-
       <ToastContainer />
     </div>
   );
